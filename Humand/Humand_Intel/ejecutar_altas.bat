e80d89bc929554e0d64892d6053efb2340c5c5c4
DesarrolloNavitek <joserivera@navitek.com.mx>
2026-09-24 16:51:58 -0600

    Delete Humand/Humand_Intel directory

--- Humand/Humand_Intel/ejecutar_altas.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo ===== %date% %time% ===== >> log_altas.txt
-python sync_altas_bajas_humand.py --solo-altas --confirmar >> log_altas.txt 2>&1
-echo. >> log_altas.txt
--- Humand/Humand_Intel/ejecutar_eliminaciones.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo ===== %date% %time% ===== >> log_eliminaciones.txt
-python sync_altas_bajas_humand.py --solo-eliminar --confirmar >> log_eliminaciones.txt 2>&1
-echo. >> log_eliminaciones.txt
